41e8e5980fee97c473a141d64b380331e650301a
sanjithpk <sanjithpk06@gmail.com>
2019-06-07 13:41:36 +0530

    Updates

---
+++ precache-manifest.1dba32a24822ca14d13cf7d32c94e9c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29f3ee5f760a8d928d5f50d2c379541",
+    "url": "/quiz-platform/index.html"
+  },
+  {
+    "revision": "d909a9d279c58583cd7f",
+    "url": "/quiz-platform/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/quiz-platform/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d909a9d279c58583cd7f",
+    "url": "/quiz-platform/static/js/main.0d0e3df0.chunk.js"
+  },
+  {
+    "revision": "900c1538031795a97ff8",
+    "url": "/quiz-platform/static/js/runtime~main.a21adcba.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/quiz-platform/static/media/logo.ee7cd8ed.svg"
+  }
+]);
